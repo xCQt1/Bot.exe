a0ebcbd2d104cff5bd1b0add8616c53fe0dd0722
Till <hubbibubbi007@gmx.de>
2023-06-10 20:27:20 +0200

    - tictactoe: Erkennung von Spielende gefixt, außerdem Zug-System eingeführt

--- cogs/games.py
+++ cogs/games.py
@@ -13,7 +13,7 @@ class Games(commands.GroupCog):
     @app_commands.command(name="tictactoe", description="Ein einfaches Tic-Tac-Toe mit einem anderen Spieler.")
     @app_commands.describe(player="Der zweite Spieler")
     async def tictactoe(self, i: discord.Interaction, player: discord.User):
-        view = TicTacToeView([i.user.id, player.id])
+        view = TicTacToeView([i.user.id, player.id], self.client)
         await i.response.send_message(view=view)
 
 
@@ -24,17 +24,20 @@ async def setup(client):
 class TicTacToeView(View):
     players: dict[int, str]
     symbols = ["⭕", "❌"]
-    waysToWin = [[(0,0), (0,1), (0,2)], [(1,0), (1,1), (1,2)], [(2,0), (2,1), (2,2)], # horizontal
-                 [(0,0), (1,0), (2,0)], [(1,0), (1,1), (1,2)], [(2,0), (2,1), (2,2)], # vertikal
-                 [(0,0), (1,1), (2,2)], [(2,0), (1,1), (0,2)]] # diagonal
+    waysToWin = [[(0,0), (0,1), (0,2)], [(1,0), (1,1), (1,2)], [(2,0), (2,1), (2,2)],  # horizontal
+                 [(0,0), (1,0), (2,0)], [(1,0), (1,1), (1,2)], [(2,0), (2,1), (2,2)],  # vertikal
+                 [(0,0), (1,1), (2,2)], [(2,0), (1,1), (0,2)]]  # diagonal
     buttons = []
+    playerPrev: discord.Member.id
 
-    def __init__(self, players: list[discord.Member.id]):
+    def __init__(self, players: list[discord.Member.id], client: discord.Client):
         super().__init__(timeout=None)
+        self.client = client
         self.players = {
             players[0]: self.symbols[0],
             players[1]: self.symbols[1]
         }
+        self.playerPrev = players[1]
         for i, player in enumerate(self.players):
             self.players[player] = self.symbols[i]
         for i in range(0, 3):
@@ -47,18 +50,23 @@ class TicTacToeView(View):
 
     async def handleTurn(self, i: discord.Interaction):
         end = False
+        self.playerPrev = i.user.id
         for way in self.waysToWin:
             emoji = self.buttons[way[0][0]][way[0][1]].emoji
             if all(self.buttons[coord[0]][coord[1]].emoji == emoji for coord in way):
-                end = True #if emoji in self.symbols else False
+                end = True if self.buttons[way[0][0]][way[0][1]].clicked else False
         if end:
             for buttons in self.buttons:
                 for button in buttons:
                     button.disabled = True
         await i.response.edit_message(view=self)
+        if end:
+            await i.followup.send(f"{self.client.get_user(self.playerPrev).name} hat das Spiel gewonnen!")
 
 
 class GameButton(Button):
+
+    clicked = False
 
     def __init__(self, gameView: TicTacToeView, row: int):
         super().__init__(row=row, style=ButtonStyle.grey, emoji="⬜")
@@ -68,6 +76,10 @@ class GameButton(Button):
         if i.user.id not in self.gameView.players:
             await i.response.send_message("Du nimmst nicht an diesem Spiel teil. Wenn du auch spielen möchtest, guck dir doch mal /games an.", ephemeral=True)
             return
+        elif i.user.id is self.gameView.playerPrev:
+            await i.response.send_message("Bitte warte bis zu deinem Zug.", ephemeral=True)
+            return
+        self.clicked = True
         self.emoji = self.gameView.players[i.user.id]
         self.disabled = True
         await self.gameView.handleTurn(i)
